15a6f9af506d41feec704cd3da5a5b6c8d183256
dbrace <davidbrace@gmail.com>
2017-09-20 00:05:12 -0400

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path:str):
+    # TODO: generate the database
+    pass
+
+
+def save_words_to_database(database_path: str, words_list: list):
+    # TODO: save words to the database
+    pass
