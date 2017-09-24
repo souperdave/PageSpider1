1876c918d002ce837593b42bc4dc25fe25de1aea
dbrace <davidbrace@gmail.com>
2017-09-23 20:25:45 -0400

    Successful big_word_list populate from urls

--- page_spider.py
+++ page_spider.py
@@ -1,16 +1,24 @@
 import os
 import argparse
+from utilities import url_utilities
 
 
 def main(database: str, url_list_file: str):
+    big_word_list = []
     print("we are going to work with " + database)
     print("we are going to scan " + url_list_file)
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print("reading " + url)
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_contents = page_content)
+        big_word_list.extend(words)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-db","--database",help="SQLit File Name")
-    parser.add_argument("-i","--input",help="Input File contraining urls to read")
+    parser.add_argument("-db","--database",help="SQLite File Name")
+    parser.add_argument("-i","--input",help="Input File containing urls to read")
     args = parser.parse_args()
     database_file= args.database
     input_file = args.input
